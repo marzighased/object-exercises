4eb6921ad417d1fa7e3102b8e7cec0f1a58bbbdc
Marzi Ghasedian <marzighased@gmail.com>
2025-08-20 23:42:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Marzi',
+    job: 'web developer',
+    family: 'ghasedian',
+    hasDrivingLicence: true,
+    friends: ['bahar', 'hadis', 'eli']
+}
+console.log(person.job)
+console.log(person['friends'])
